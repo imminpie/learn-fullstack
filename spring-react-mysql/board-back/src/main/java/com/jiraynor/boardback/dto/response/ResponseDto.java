b2d4e99d00f8c089f0ba2930f3c76c5a35833687
imminpie <mjkang0715@gmail.com>
2024-02-01 22:41:27 +0900

    feat: Response 코드 및 메시지 인터페이스 작성

---
+++ spring-react-mysql/board-back/src/main/java/com/jiraynor/boardback/dto/response/ResponseDto.java
@@ -0,0 +1,23 @@
+package com.jiraynor.boardback.dto.response;
+
+import com.jiraynor.boardback.common.ResponseCode;
+import com.jiraynor.boardback.common.ResponseMessage;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@AllArgsConstructor
+public class ResponseDto {
+
+    // 모든 REST API 응답은 코드와 메세지를 포함한다.
+    private String code;
+    private String message;
+
+    public static ResponseEntity<ResponseDto> databaseError() {
+        ResponseDto responseBody = new ResponseDto(ResponseCode.DATABASE_ERROR, ResponseMessage.DATABASE_ERROR);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(responseBody);
+    }
+
+}
