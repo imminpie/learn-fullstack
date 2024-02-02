b841922966782889dba9386c7bba4364aa6d1616
imminpie <mjkang0715@gmail.com>
2024-02-02 22:35:59 +0900

    feat: 컴포넌트 생성 (게시물 리스트 아이템)

---
+++ spring-react-mysql/board-front/src/components/BoardItem/index.tsx
@@ -0,0 +1,49 @@
+import { BoardListItem } from '../../types/interface';
+import defaultProfileImage from 'assets/image/default-profile-image.png';
+import './style.css';
+
+interface Props {
+  boardListItem: BoardListItem;
+}
+
+export default function BoardItem({ boardListItem }: Props) {
+  const { boardNumber, title, content, boardTitleImage } = boardListItem;
+  const { favoriteCount, commentCount, viewCount } = boardListItem;
+  const { writeDatetime, writerNickName, writerProfileImage } = boardListItem;
+
+  return (
+    <div className='board-list-item'>
+      <div className='board-list-item-main-box'>
+        <div className='board-list-item-top'>
+          <div className='board-list-item-profile-box'>
+            <div
+              className='board-list-item-profile-image'
+              style={{
+                backgroundImage: `url(${
+                  writerProfileImage ? writerProfileImage : defaultProfileImage
+                })`,
+              }}
+            ></div>
+          </div>
+          <div className='board-list-item-write-box'>
+            <div className='board-list-item-nickname'>{writerNickName}</div>
+            <div className='board-list-item-write-datetime'>{writeDatetime}</div>
+          </div>
+        </div>
+        <div className='board-list-item-middle'>
+          <div className='board-list-item-title'>{title}</div>
+          <div className='board-list-item-content'>{content}</div>
+        </div>
+        <div className='board-list-item-bottom'>
+          <div className='board-list-item-counts'>{`댓글 ${commentCount} · 좋아요 ${favoriteCount} · 조회수 ${viewCount}`}</div>
+        </div>
+      </div>
+      <div className='board-list-item-image-box'>
+        <div
+          className='board-list-item-image'
+          style={{ backgroundImage: `url(${boardTitleImage})` }}
+        ></div>
+      </div>
+    </div>
+  );
+}
---
+++ spring-react-mysql/board-front/src/mocks/latest-board-list.mock.ts
@@ -0,0 +1,42 @@
+import { BoardListItem } from 'types/interface';
+
+const latestBoardListMock: BoardListItem[] = [
+  {
+    boardNumber: 1,
+    title: '제목입니다1',
+    content: '내용입니다1',
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2023.08.18. 00:54:27',
+    writerNickName: '닉네임1',
+    writerProfileImage: null,
+  },
+  {
+    boardNumber: 2,
+    title: '제목입니다2',
+    content: '내용입니다2',
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2023.08.18. 00:54:27',
+    writerNickName: '닉네임2',
+    writerProfileImage: null,
+  },
+  {
+    boardNumber: 3,
+    title: '제목입니다3',
+    content: '내용입니다3',
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2023.08.18. 00:54:27',
+    writerNickName: '닉네임3',
+    writerProfileImage: null,
+  },
+];
+
+export { latestBoardListMock };
