de5c242b0fe2c595f7877c14870f4c56ebe47302
imminpie <mjkang0715@gmail.com>
2024-02-02 22:45:26 +0900

    feat: 컴포넌트 생성 (Top 3 리스트 아이템)

---
+++ spring-react-mysql/board-front/src/mocks/top-3-board-list.mock.ts
@@ -0,0 +1,42 @@
+import { BoardListItem } from 'types/interface';
+
+const top3BoardListMock: BoardListItem[] = [
+  {
+    boardNumber: 1,
+    title: '제목입니다1',
+    content: '내용입니다1',
+    boardTitleImage: 'https://cdn.eyesmag.com/content/uploads/posts/2022/03/21/gyeongbokgung-palace-night-01-1408c7c5-b5f5-4554-b3dc-7f3be6f75acc.jpg',
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2023.08.18. 00:54:27',
+    writerNickName: '닉네임1',
+    writerProfileImage: null,
+  },
+  {
+    boardNumber: 2,
+    title: '제목입니다2',
+    content: '내용입니다2',
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2023.08.18. 00:54:27',
+    writerNickName: '닉네임2',
+    writerProfileImage: null,
+  },
+  {
+    boardNumber: 3,
+    title: '제목입니다3',
+    content: '내용입니다3',
+    boardTitleImage: null,
+    favoriteCount: 0,
+    commentCount: 0,
+    viewCount: 0,
+    writeDatetime: '2023.08.18. 00:54:27',
+    writerNickName: '닉네임3',
+    writerProfileImage: null,
+  },
+];
+
+export { top3BoardListMock };
