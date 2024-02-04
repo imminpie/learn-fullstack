753ff85de910e418e7706de44717531272b13af1
imminpie <mjkang0715@gmail.com>
2024-02-04 22:27:31 +0900

    feat: Auth 페이지 공통 스타일 작성

--- spring-react-mysql/board-front/src/views/Authentication/index.tsx
+++ spring-react-mysql/board-front/src/views/Authentication/index.tsx
@@ -1,5 +1,32 @@
 import './style.css';
+import { useState } from 'react';
 
 export default function Authentication() {
-  return <div>인증 화면</div>;
+  const [view, setView] = useState<'sign-in' | 'sign-up'>('sign-in');
+
+  const SignInCard = () => {
+    return <div className='auth-card'></div>;
+  };
+
+  const SignUpCard = () => {
+    return <div className='auth-card'></div>;
+  };
+
+  return (
+    <div className='auth-wrapper'>
+      <div className='auth-container'>
+        <div className='auth-jumbotron-box'>
+          <div className='auth-jumbotron-contents'>
+            <div className='auth-logo-icon'></div>
+            <div className='auth-jumbotron-text-box'>
+              <div className='auth-jumbotron-text'>환영합니다.</div>
+              <div className='auth-jumbotron-text'>Minpie Board 입니다.</div>
+            </div>
+          </div>
+        </div>
+        {view === 'sign-in' && <SignInCard />}
+        {view === 'sign-up' && <SignUpCard />}
+      </div>
+    </div>
+  );
 }
