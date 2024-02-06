fce5a6d06cde81235b844a7937cf09211a56cff3
imminpie <mjkang0715@gmail.com>
2024-02-06 16:29:01 +0900

    feat: 게시물 작성 페이지 구조 작성

--- spring-react-mysql/board-front/src/views/Board/Write/index.tsx
+++ spring-react-mysql/board-front/src/views/Board/Write/index.tsx
@@ -1,5 +1,56 @@
+import { useEffect, useRef, useState } from 'react';
 import './style.css';
+import { useBoardStore } from 'stores';
 
 export default function Write() {
-  return <div>게시글 작성</div>;
+  const { title, setTitle } = useBoardStore();
+  const { content, setContent } = useBoardStore();
+  const { boardImageFileList, setBoardImageFileList } = useBoardStore();
+  const { resetBoard } = useBoardStore();
+  const [imageUrls, setImageUrls] = useState<string[]>([]);
+
+  const contentRef = useRef<HTMLTextAreaElement | null>(null);
+  const imageInputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    resetBoard();
+  }, []);
+
+  return (
+    <div className='board-write-wrapper'>
+      <div className='board-write-container'>
+        <div className='board-write-box'>
+          <div className='board-write-title-box'>
+            <input
+              className='board-write-title-input'
+              type='text'
+              placeholder='제목을 작성해 주세요.'
+              value={title}
+            />
+          </div>
+          <div className='divider'></div>
+          <div className='board-write-content-box'>
+            <textarea
+              ref={contentRef}
+              className='board-write-content-textarea'
+              placeholder='본문을 작성해 주세요.'
+              value={content}
+            ></textarea>
+            <div className='icon-button'>
+              <div className='icon image-box-light-icon'></div>
+            </div>
+            <input ref={imageInputRef} type='file' accept='image/*' style={{ display: 'none' }} />
+          </div>
+          <div className='board-write-images-box'>
+            <div className='board-write-image-box'>
+              <div className='board-write-image'></div>
+              <div className='icon-button image-close'>
+                <div className='icon close-icon'></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
