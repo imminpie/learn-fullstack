2f9481b536916e4ee084eae847f40c8bb5668375
imminpie <mjkang0715@gmail.com>
2024-03-19 15:43:27 +0900

    feat: MariaDB 와 부트 프로젝트 생성

---
+++ spring-react/mall-back/src/main/java/org/zerock/mallback/MallBackApplication.java
@@ -0,0 +1,13 @@
+package org.zerock.mallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallBackApplication.class, args);
+	}
+
+}
---
+++ spring-react/mall-back/src/test/java/org/zerock/mallback/MallBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zerock.mallback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
